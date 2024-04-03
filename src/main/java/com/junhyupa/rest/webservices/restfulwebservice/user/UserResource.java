134d8f050b57e650a0d2279164c128d8582da2d0
jun <yoseb95@gmail.com>
2024-04-03 14:29:08 +0900

    Feat: /users /user/{id} API 개발

---
+++ src/main/java/com/junhyupa/rest/webservices/restfulwebservice/user/UserResource.java
@@ -0,0 +1,28 @@
+package com.junhyupa.rest.webservices.restfulwebservice.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private UserDaoService userDaoService;
+
+    public UserResource(UserDaoService userDaoService) {
+        this.userDaoService = userDaoService;
+    }
+    //Get /users
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUsersById(@PathVariable int id) {
+        return userDaoService.findOne(id);
+    }
+}
